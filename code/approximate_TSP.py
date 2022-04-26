5d135336d6df3a8962dc55442b4e11aac3efb610
Blake Jones <jones2bg@dukes.jmu.edu>
2022-04-26 18:42:19 -0400

    feat: Approximate boilerplate

---
+++ code/approximate_TSP.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Author(s): Blake Jones
+# Description: This script creates an approximation of the TSP problem.
+# Date: 4/26/22
+
+import random
+import time
+
+def approximate(start_node, runtime, graph):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
