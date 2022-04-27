0db81732acd01b76cddc63e32d941d3f269f2523
QC8086 <QC8086@gmail.com>
2022-04-27 18:14:07 -0400

    Add a script to run and time exact_TSP on all inputs.

---
+++ code/run_exact.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+small=($(ls small))
+medium=($(ls medium))
+large=($(ls large))
+
+for item in ${small[@]}
+do
+  printf "small/$item\n"
+  time ./exact_TSP.py < small/$item
+  printf "\n"
+done
+
+for item in ${medium[@]}
+do
+  printf "medium/$item\n"
+  time ./exact_TSP.py < medium/$item
+  printf "\n"
+done
+
+for item in ${large[@]}
+do
+  printf "large/$item\n"
+  time ./exact_TSP.py < large/$item
+  printf "\n"
+done
