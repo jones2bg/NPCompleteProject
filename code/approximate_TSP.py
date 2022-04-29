f9c1fb7d4e0d73c742f433d5331a21079a77aeba
QC8086 <QC8086@gmail.com>
2022-04-29 17:26:48 -0400

    Change approximate_TSP to print out its state after an interval.

--- code/approximate_TSP.py
+++ code/approximate_TSP.py
@@ -9,48 +9,65 @@ import time
 import sys
 
 
-def approximate(start_node, runtime, graph, nodes):
-    # hold our current start time. 
+def approximate(start_node, runtime, interval, graph, nodes):
     start_time = time.time()
 
-    # generate a random path from the nodes
+    # Generate a random path from the nodes.
     path = list(nodes[start_node])
     no_start_end_path = list(nodes[start_node])
-    # shuffle the nodes
+
+    # Shuffle the nodes.
     random.shuffle(path)
-    # add the start to the front and end of the path.
+
+    # Add the start to the front and end of the path.
     path.insert(0, start_node)
     path.append(start_node)
-    
+
     curr_min = sys.maxsize
-    # amount of paths taken.
+    # Amount of paths taken.
     counter = 1
-    
-    # run our approximation for (runtime) seconds
+
+    next_interval = time.time() + (interval / 1000)
+    current_interval = interval
+
+    # Run our approximation for (runtime) seconds.
     while time.time() < start_time + (runtime / 1000):
+        if (time.time() > next_interval):
+            print("Runtime:", current_interval)
+            print("Minimum Cost Path:")
+            print(path)
+            print("Minimum Cost:", curr_min)
+            print("Paths evaluated:", counter)
+            print()
+            next_interval = time.time() + (interval / 1000)
+            current_interval += interval
+
         cost = 0
         for i in range(len(path) - 1):
             cost += graph[(path[i], path[i+1])]
         if cost < curr_min:
             curr_min = cost
-        
+
         # sample two random items in the list.
         choices = random.sample(no_start_end_path, 2)
         index_first_ele = path.index(choices[0])
         index_second_ele = path.index(choices[1])
-        
+
         # create the new path
         path[index_first_ele], path[index_second_ele] = path[index_second_ele], path[index_first_ele]
         counter += 1
-        
+
+    print("Runtime:", current_interval)
     print("Minimum Cost Path:")
     print(path)
     print("Minimum Cost:", curr_min)
-    print("Cumulative Amount of Paths Evaluated:", counter)
+    print("Paths evaluated:", counter)
 
-# prompt for input and approximate the minima.
 def main():
+    # Amount of time to run in miliseconds
     runtime = int(sys.argv[1])
+    # Interval to print minimum path found.
+    interval = int(sys.argv[2])
 
     v, e = [int(x) for x in input().split(' ')[:2]]
     edges = [[int(x) for x in input().split(' ')] for _ in range(0, e)]
@@ -58,15 +75,15 @@ def main():
 
     graph = dict()
     nodes = dict()
-    
+
     for i in range(v):
         nodes[i] = set()
 
     for edge in edges:
         nodes[edge[0]].add(edge[1])
         graph[(edge[0], edge[1])] = edge[2]
-    
-    approximate(start_node, runtime, graph, nodes)
+
+    approximate(start_node, runtime, interval, graph, nodes)
 
 if __name__ == "__main__":
     main()
--- code/run_approximate.sh
+++ code/run_approximate.sh
@@ -4,37 +4,29 @@ small=($(ls small))
 medium=($(ls medium))
 large=($(ls large))
 
-times_=(1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000)
+time=3000
+interval=100
 
-for time in ${times_[@]}
-  do
-  for item in ${small[@]}
-  do
-    printf "small/$item\n"
-    printf "time/$time\n"
-    ./approximate_TSP.py $time < small/$item
-    printf "\n"
-  done
+for item in ${small[@]}
+do
+  printf "small/$item\n"
+  printf "time/$time\n"
+  ./approximate_TSP.py $time $interval < small/$item
+  printf "\n"
 done
 
-for time in ${times_[@]}
-  do
-  for item in ${medium[@]}
-  do
-    printf "medium/$item\n"
-    printf "time/$time\n"
-    time ./approximate_TSP.py $time < medium/$item
-    printf "\n"
-  done
+for item in ${medium[@]}
+do
+  printf "medium/$item\n"
+  printf "time/$time\n"
+  time ./approximate_TSP.py $time $interval < medium/$item
+  printf "\n"
 done
 
-for time in ${times_[@]}
-  do
-  for item in ${large[@]}
-  do
-    printf "large/$item\n"
-    printf "time/$time\n"
-    time ./approximate_TSP.py $time < large/$item
-    printf "\n"
-  done
+for item in ${large[@]}
+do
+  printf "large/$item\n"
+  printf "time/$time\n"
+  time ./approximate_TSP.py $time $interval < large/$item
+  printf "\n"
 done
