cc6553c2da6990c023193f9411677447ba152dce
QC8086 <QC8086@gmail.com>
2022-05-01 10:38:32 -0400

    Move exact solution to appropriate place.

---
+++ exact_solution/README.md
@@ -0,0 +1,7 @@
+# Exact Solution
+
+## Running All Test Cases
+Run run_test_cases.sh with no arguments to run all provided test cases.
+
+## Running Individually
+Run exact_TSP.py and provide an input graph and a selected starting node on stdin.
--- exact_solution/run_test_cases.sh
+++ exact_solution/run_test_cases.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-small=($(ls small))
-medium=($(ls medium))
-large=($(ls large))
+small=($(ls test_cases/small))
+medium=($(ls test_cases/medium))
+large=($(ls test_cases/large))
 
 for item in ${small[@]}
 do
   printf "small/$item\n"
-  ./exact_TSP.py < small/$item
+  ./exact_TSP.py < test_cases/small/$item
   printf "\n"
 done
 
 for item in ${medium[@]}
 do
   printf "medium/$item\n"
-  ./exact_TSP.py < medium/$item
+  ./exact_TSP.py < test_cases/medium/$item
   printf "\n"
 done
 
 for item in ${large[@]}
 do
   printf "large/$item\n"
-  ./exact_TSP.py < large/$item
+  ./exact_TSP.py < test_cases/large/$item
   printf "\n"
 done
